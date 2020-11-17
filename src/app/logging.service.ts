7e8fe99d9a2c452ae7ee9c35f5d2e5056e7e24f7
jayme.weiss <jayme.weiss@entelect.co.za>
2020-11-17 15:27:22 +0200

    add services

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,11 +1,7 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
 export class LoggingService {
 
-  constructor() { }
+  constructor() {
+  }
 
   logStatusChange(status: string) {
     console.log('A server status changed, new status: ' + status);
